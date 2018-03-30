e2718a4f24762484c2e705b3dd286ab80f2f76b4
Playingtygre <kymiehsu123@gmail.com>
2018-03-29 21:43:27 -0700

    moving jSon folder

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello World!");
+
+            string mypath = "..//data.json";
+            Fil
+           
+      
+        }
+
+        public static void ReadFile(string mypath)
+        {
+            try
+            {
+                using (StreamReader sr = File.OpenText(mypath))
+                {
+                    string s = " ";
+                    while ((s = sr.ReadLine() != null))
+                    {
+                        Console.WriteLine()
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+       
+
+
+
+     
+
+    }
+}
