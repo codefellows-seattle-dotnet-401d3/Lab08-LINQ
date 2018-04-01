1fa56a7faff48c1db43f2be8cf17a2a696b8cb88
Playingtygre <kymiehsu123@gmail.com>
2018-03-31 20:41:38 -0700

    Seperating classes

--- LINQ/LINQ/FeaturesCollection.cs
+++ LINQ/LINQ/FeaturesCollection.cs
@@ -19,22 +19,6 @@ namespace LINQManhattan
         public Properties properties { get; set; }
     }
 
-    public class Geometry
-    {
-        public string type { get; set; }
-        public float[] coordinates { get; set; }
-    }
-
-    public class Properties
-    {
-        public string zip { get; set; }
-        public string city { get; set; }
-        public string state { get; set; }
-        public string address { get; set; }
-        public string borough { get; set; }
-        public string neighborhood { get; set; }
-        public string county { get; set; }
-    }
 }
 
 
---
+++ LINQ/LINQ/Geometry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace LINQManhattan
+{
+    public class Geometry
+    {
+        public string Type { get; set; }
+
+        [JsonProperty]
+        public double[] coordinates { get; set; }
+    }
+}
