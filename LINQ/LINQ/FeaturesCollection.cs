83ef421a7706c8a93bc594122b4cfb605ca15b58
Playingtygre <kymiehsu123@gmail.com>
2018-03-31 19:47:23 -0700

    working String file path

--- LINQ/LINQ/FeaturesCollection.cs
+++ LINQ/LINQ/FeaturesCollection.cs
@@ -5,14 +5,34 @@ using Newtonsoft.Json;
 
 namespace LINQManhattan
 {
-    [JsonObject(MemberSerialization.OptIn)]
 
-    public class FeaturesCollection
+    public class JSONTransferObject
     {
-        [JsonProperty]
-        public String Type {get; set;}
+        public string type { get; set; }
+        public Feature[] features { get; set; }
+    }
 
-        [JsonProperty]
-        public List<Features> Features { get; set;}
+    public class Feature
+    {
+        public string type { get; set; }
+        public Geometry geometry { get; set; }
+        public Properties properties { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string type { get; set; }
+        public float[] coordinates { get; set; }
+    }
+
+    public class Properties
+    {
+        public string zip { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string address { get; set; }
+        public string borough { get; set; }
+        public string neighborhood { get; set; }
+        public string county { get; set; }
     }
 }
--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -1,8 +1,10 @@
-﻿using Newtonsoft.Json;
+﻿using LINQManhattan;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 /*
  * using NewtonSoft to turn JSON data into Generics collection
@@ -12,33 +14,59 @@ namespace LINQ
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            /* Creating a string this is the start 
-             */
-            string manhattan;
-            using (StreamReader r = new StreamReader("data.json"))
+
+            static void Main(string[] args)
             {
-                manhattan = r.ReadToEnd();
-            };
-            FeaturesCollection items = 
-                JsonConvert.DeserializeObject<FeaturesCollection>(manhattan);
-            Console.WriteLine();
+                string filePath = "../../../data.json";
+                StreamReader sr = new StreamReader(filePath);
 
-            Console.WriteLine(manhattan);
+                var jObject = JsonConvert.DeserializeObject <JSONTransferObject>(sr.ReadLine());
+                //Print all neighborhoods
 
-
-    
-           
-      
-        }
-
-       
-       
+                foreach (var item in jObject.features)
+                {
+                    Console.WriteLine(item.properties.neighborhood);
+                Console.ReadLine();
 
 
 
-     
+            }
 
+                
+
+            /*
+                //Filter out the empty?
+                IEnumerable<string> neighList = from feat in jObject.features
+                                                where feat.properties.neighborhood != null
+                                                select feat.properties.neighborhood;
+
+                foreach (string item in neighList)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.ReadLine();
+                //Filter out the dupes
+                IEnumerable<string> filterDup = neighList.Distinct();
+                foreach (var item in filterDup)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.ReadLine();
+                //Combine the queries into one
+                IEnumerable<string> fullFilter = from item in jObject.features
+                                                 where item.properties.neighborhood != null
+                                                 select item.properties.neighborhood;
+                //Lamda of the previous filter. It reaturns a feature though.
+                IEnumerable<Feature> fullLamda =
+                    jObject.features.Where(item => item.properties.neighborhood != null).Distinct();
+                foreach (var item in fullFilter.Distinct())
+                {
+                    Console.WriteLine(item);
+                }
+                Console.ReadLine();
+                */
+            }
+            
+        
     }
 }
